7643c2f26d91b4f1a841de3900d9cf26649f7314
swatikanishu <nishuswatika@gmail.com>
2022-07-24 22:03:39 +0530

    Added Welcome message

---
+++ src/main/java/com/censusanalyser/InvoiceGenerator.java
@@ -0,0 +1,8 @@
+package com.censusanalyser;
+
+public class InvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Cab Invoice Generator Program");
+
+    }
+}
